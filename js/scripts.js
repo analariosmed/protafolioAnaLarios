51722b21b919abfaababa460c01e8e944f065148
analariosmed <ana.lariosma@gmail.com>
2024-10-08 14:42:12 -0400

    altura protafolio fixed

--- js/scripts.js
+++ js/scripts.js
@@ -51,14 +51,14 @@ window.addEventListener('DOMContentLoaded', event => {
         const card = document.createElement('div');
         card.className = 'col-md-4';
         card.innerHTML = `
-                <div class="card">
-                    <img src="${backend}${project.image}" alt="${project.name}" class="card-img-top">
-                    <div class="card-body">
-                        <h5 class="card-title">${project.name}</h5>
-                        <p class="card-text">${project.description}</p>
-                        <a href="${project.url}" class="btn btn-github">View on GitHub</a>
-                    </div>
-                </div>           
+            <div class="card h-100">
+                <img src="${backend}${project.image}" alt="${project.name}" class="card-img-top">
+                <div class="card-body">
+                <h5 class="card-title">${project.name}</h5>
+                <p class="card-text">${project.description}</p>
+                <a href="${project.url}" class="btn btn-github">View on GitHub</a>
+                </div>
+            </div>           
         `;
         return card;
     }
